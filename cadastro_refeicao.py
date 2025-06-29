6213b5df2457a35cd031d31bd87035307b43492f
Luís Gustavo <luis.araujo.sesisenaisp@gmail.com>
2025-06-29 15:41:23 -0300

    Tabela com ML aplicado

--- cadastro_refeicao.py
+++ cadastro_refeicao.py
@@ -13,23 +13,28 @@ class Aplicativo:
         
         
         self.frutas = [
-            "Banana", "Maçã", "Uva", "Pera", "Laranja", "Abacaxi",
-            "Morango", "Manga", "Melancia", "Mamão", "Kiwi",
-            "Coco", "Limão", "Framboesa"
+            "Banana", "Maçã", "Uva"
         ]
 
         self.proteinas_cafe_da_manha = [
-            "Ovo", "Leite", "Iogurte natural", "Queijo branco (ricota, minas)",
-            "Requeijão light", "Peito de peru", "Atum", "Carne de soja", "Tofu",
-            "Manteiga de amendoim (sem açúcar)", "Shake proteico (whey protein)", "Cottage"
+            "Ovo", "Leite", "Iogurte natural",  "Shake proteico (whey protein)"
         ]
 
         self.carboidratos_cafe_da_manha = [
-            "Pão francês", "Pão integral", "Tapioca", "Cuscuz", "Aveia", "Granola",
-            "Bolo simples (milho, fubá, etc.)", "Bolacha integral", "Banana (rica em carboidrato)",
-            "Batata-doce", "Inhame", "Arroz integral (p/ café reforçado)"
+            "Pão francês", "Pão integral", "Tapioca"
         ]
-
+        self.proteinas_almoco = [
+            "Picanha", "Contrafile", "Patinho"
+        ]
+        self.carboidratos_almoco = [
+            "Arroz","Macarrão","Batata Frita"
+        ]
+        self.proteinas_janta = [
+            "Frango Assado", "Salmão Grelhado", "Ovo"
+        ]
+        self.carboidratos_jantas = [
+            "Arroz", "Feijão", "Lentilha"
+        ]
         menu = st.radio("Escolha a refeição:", ["Café da Manhã", "Almoço", "Janta"], horizontal=True)
 
         if menu == "Café da Manhã":
@@ -77,12 +82,74 @@ class Aplicativo:
     def pagina_almoco(self):
         st.subheader("🍽️ Almoço")
         st.write("Aqui você pode registrar o que comeu no almoço.")
-        
+        data_almoco = st.date_input("Data de hoje")
+        horario_almoco= st.time_input("Horário da alimentação")
+
+        tipos_de_comida = ["🍌 Frutas", "🍗 Proteínas", "🍚 Carboídratos"]
+        escolha_alimentacao = st.radio("Selecione um tipo de comida:", tipos_de_comida, horizontal=True)
+        st.write(f"Você selecionou: {escolha_alimentacao}")
+
+        alimento_escolhido = None
+
+        if escolha_alimentacao == "🍌 Frutas":
+            alimento_escolhido = st.selectbox("Escolha uma fruta:", self.frutas)
+
+        elif escolha_alimentacao == "🍗 Proteínas":
+            alimento_escolhido = st.selectbox("Escolha uma proteína:", self.proteinas_almoco)
+
+        elif escolha_alimentacao == "🍚 Carboídratos":
+            alimento_escolhido = st.selectbox("Escolha um carboidrato:", self.carboidratos_almoco)
+
+        quantidade = st.number_input("Quantidade (gramas ou unidades)", step=1)
+
+        if st.button("Salvar refeição"):
+            dados = {
+                "refeicao": "Café da Manhã",
+                "tipo": escolha_alimentacao,
+                "data": str(data_almoco),
+                "horario": str(horario_almoco),
+                "alimento": alimento_escolhido,
+                "quantidade/g": quantidade
+            }
+            self.colecao.insert_one(dados)
+            st.success("Refeição salva com sucesso!")
         
 
     def pagina_janta(self):
         st.subheader("🌙 Janta")
         st.write("Aqui você pode registrar o que comeu na janta.")
+        data_janta = st.date_input("Data de hoje")
+        horario_janta= st.time_input("Horário da alimentação")
+
+        tipos_de_comida = ["🍌 Frutas", "🍗 Proteínas", "🍚 Carboídratos"]
+        escolha_alimentacao = st.radio("Selecione um tipo de comida:", tipos_de_comida, horizontal=True)
+        st.write(f"Você selecionou: {escolha_alimentacao}")
+
+        alimento_escolhido = None
+
+        if escolha_alimentacao == "🍌 Frutas":
+            alimento_escolhido = st.selectbox("Escolha uma fruta:", self.frutas)
+
+        elif escolha_alimentacao == "🍗 Proteínas":
+            alimento_escolhido = st.selectbox("Escolha uma proteína:", self.proteinas_janta)
+
+        elif escolha_alimentacao == "🍚 Carboídratos":
+            alimento_escolhido = st.selectbox("Escolha um carboidrato:", self.carboidratos_jantas)
+
+        quantidade = st.number_input("Quantidade (gramas ou unidades)", step=1)
+
+        if st.button("Salvar refeição"):
+            dados = {
+                "refeicao": "Café da Manhã",
+                "tipo": escolha_alimentacao,
+                "data": str(data_janta),
+                "horario": str(horario_janta),
+                "alimento": alimento_escolhido,
+                "quantidade/g": quantidade
+            }
+            self.colecao.insert_one(dados)
+            st.success("Refeição salva com sucesso!")
+        
 
 if __name__ == "__main__":
     Aplicativo()
--- tabela.py
+++ tabela.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from pymongo import MongoClient
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 
 
 class Utilidades:
@@ -9,11 +13,59 @@ class Utilidades:
         self.client = MongoClient("mongodb://localhost:27017/")
         self.db = self.client["nutritiondiary"]
         self.colecao = self.db["cadastre"]
+        self.dados_ml = pd.DataFrame({
+        "Alimento": ["Ovo", "Banana","Leite","Pão Integral","Maça","Uva","Iogurte natural","Shake proteico (whey protein)","Pão Frances","Tapioca","Picanha","Contrafile","Patinho","Arroz","Macarrão","Batata Frita","Frango Assado","Salmão Grelhado","Ovo","Arroz","Feijão","Lentilha"],
+        "Proteina": [13,1.3,3.3,9.5,0.36,0.6,5.25,86.58,4.00,0.7,19.72,24,21.75,2.66,5.76,5.1,28,26.1,13,2.66,4.8,6.3],
+        "Carboidrato":[1.1,23,4.8,40,25,18,7.4,1.22,29.45,36,0,0,0,27.9,30.68,35.6,0,0,1.1,27.9,5.1,16.3]
+        })
+        self.modelo = self.treinar_modelo()
+    
+    def treinar_modelo(self):
+        X = pd.DataFrame({
+            "Alimento":self.dados_ml["Alimento"],
+            "Quantidade":[100]*len(self.dados_ml)
+        })
+        y=self.dados_ml[["Proteina","Carboidrato"]]
+        preprocessor = ColumnTransformer(transformers=[
+            ("onehot",OneHotEncoder(),["Alimento"])
+        ],remainder="passthrough")
+
+        pipeline = Pipeline(steps=[
+            ("preprocessador",preprocessor),
+            ("regressor",LinearRegression())
+        ])
+        pipeline.fit(X,y)
+        return pipeline
+    def prever_nutrientes(self,alimento,quantidade):
+        entrada = pd.DataFrame([{"Alimento":alimento, "Quantidade":100}])
+        try:
+            resultado = self.modelo.predict(entrada)
+            fator = quantidade/100
+            proteina = round(resultado[0][0]* fator,2)
+            carboidrato = round(resultado[0][1]*fator,2)
+        except:
+            proteina = carboidrato = None
+        return proteina,carboidrato
+
+    
     def mostrar_tabela_de_refeicao(self):
         dados = list(self.colecao.find({},{'_id':0}))
         if dados:
             df = pd.DataFrame(dados)
-            st.dataframe(df,use_container_width=True)
+            proteinas_previstas = []
+            carboidratos_previstos = []
+
+            for _, row in df.iterrows():
+                alimento = row.get("alimento")
+                quantidade = row.get("quantidade/g", 0)
+                p, c = self.prever_nutrientes(alimento, quantidade)
+                proteinas_previstas.append(p)
+                carboidratos_previstos.append(c)
+
+            df["Proteína (g) estimada"] = proteinas_previstas
+            df["Carboidrato (g) estimado"] = carboidratos_previstos
+
+            st.dataframe(df, use_container_width=True)
         else:
             st.info("Nenhuma refeição cadastrada")
         
